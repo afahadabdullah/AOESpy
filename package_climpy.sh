5aa56658b19cc913ccba84ca585dc8a9714f0f36
fahadabdullaah <41709931+fahadabdullaah@users.noreply.github.com>
2019-05-25 02:08:52 -0400

    Add files via upload

---
+++ package_climpy.sh
@@ -0,0 +1,10 @@
+#@afahad@gmu.edu
+#May 25, 2019
+#install necessary packages before
+
+python -m pip install -U pip setuptools
+python -m pip install matplotlib
+pip install numpy --user
+pip install scipy --user
+pip install netCDF4 --user
+
