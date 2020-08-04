bb338bd9036e0d297a98cfdcdbb24c5981bc35b7
Abdullah Al Fahad <41709931+afahadabdullah@users.noreply.github.com>
2020-08-04 12:29:15 -0400

    Add files via upload

--- aoespy.py
+++ aoespy.py
@@ -129,8 +129,14 @@ def ltrend(var,lon,lat,time,sig=False):
 	#			m= 1 (default) plots meridioinal line, or 0 doesn't plot 
 
 
-def plot(var,lon,lat,title='',clf=[],cl=[], cmap='coolwarm',lon1=-180,lon2=180,lat1=-90,lat2=90,bar=1,p=1,m=1,lmask=0, alpha=.95):
+def plot(var,lon=[],lat=[],title='',clf=[],cl=[], cmap='coolwarm',lon1=-180,lon2=180,lat1=-90,lat2=90,bar=1,p=1,m=1,lmask=0, alpha=.95):
 
+    if len(lon)<1:
+        lon=var.lon;
+
+    if len(lat)<1:
+        lat=var.lat;
+            
     if lon1== -180:
         if lon2==180:
             if nanmax(lon)>181:
@@ -151,8 +157,15 @@ def plot(var,lon,lat,title='',clf=[],cl=[], cmap='coolwarm',lon1=-180,lon2=180,l
                 llcrnrlon=lon1,urcrnrlon=lon2,resolution='l')
 
     map.drawcoastlines(linewidth=.6,)
+
     parallels = arange(lat1,lat2+1, (lat2-lat1)//4) 
     meridians = arange(lon1,lon2,(lon2-lon1)//4)
+    
+    if nanmax(lat)-nanmin(lat)>89:
+        parallels = arange(-90,90+1,30)
+
+    if nanmax(lon)-nanmin(lon)>179:
+        meridians = arange(0,360+1,60)
 
     if lmask==1:
         map.fillcontinents(color='white', lake_color='white', ax=None, zorder=None, alpha=None)
@@ -173,30 +186,30 @@ def plot(var,lon,lat,title='',clf=[],cl=[], cmap='coolwarm',lon1=-180,lon2=180,l
     if len(cl)==1:
 
         if len(clf)>1:
-            csf = map.contourf(x,y,var,clf,extend='both',cmap=cmap, alpha=.95)
+            csf = map.contourf(x,y,var,clf,extend='both',cmap=cmap, alpha=alpha)
             if bar==1:
                 cb = map.colorbar(csf,"bottom", extend='both',size="3%", pad="12%")
-            cs = map.contour(x,y,var,cl,colors='k',linewidths=0.3, alpha=.95)
+            cs = map.contour(x,y,var,cl,colors='k',linewidths=0.3, alpha=alpha)
             #plt.clabel(cs, inline=True, fmt='%1.1f', fontsize=6, colors='k')
             plt.title(title,fontsize=9) 
         else:
-            csf = map.contourf(x,y,var,extend='both',cmap=cmap, alpha=.95)
+            csf = map.contourf(x,y,var,extend='both',cmap=cmap, alpha=alpha)
             #cb = map.colorbar(csf,"bottom", extend='both',size="3%", pad="9%")
             if bar==1:
                 cb = map.colorbar(csf,"bottom", extend='both',size="3%", pad="12%")
-            cs = map.contour(x,y,var,colors='k',linewidths=0.3, alpha=.95)
+            cs = map.contour(x,y,var,colors='k',linewidths=0.3, alpha=alpha)
             #plt.clabel(cs, inline=True, fmt='%1.1f', fontsize=6, colors='k')
             plt.title(title,fontsize=9)
     else:
 
         if len(clf)>1:
-            csf = map.contourf(x,y,var,clf,extend='both',cmap=cmap, alpha=.95)
+            csf = map.contourf(x,y,var,clf,extend='both',cmap=cmap, alpha=alpha)
             if bar==1:
                 cb = map.colorbar(csf,"bottom", extend='both',size="5%", pad="15%")
             #plt.clabel(cs, inline=True, fmt='%1.1f', fontsize=6, colors='k')
             plt.title(title,fontsize=9) 
         else:
-            csf = map.contourf(x,y,var,extend='both',cmap=cmap, alpha=.95)
+            csf = map.contourf(x,y,var,extend='both',cmap=cmap, alpha=alpha)
             #cb = map.colorbar(csf,"bottom", extend='both',size="3%", pad="9%")
             if bar==1:
                 cb = map.colorbar(csf,"bottom", extend='both',size="3%", pad="12%")
@@ -843,4 +856,107 @@ def test():
     f()
     plot(sst,lon,lat, 'Test AOESpy annual mean SST plot from Era-interim data', cmap=cmap(), lat1=-89, lat2=89)
     d()
+
+## routines for calculating seasonal climatology and seasonal timeseries
+import xarray as xr
+import numpy as np
+
+dpm = {'noleap': [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31],
+       '365_day': [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31],
+       'standard': [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31],
+       'gregorian': [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31],
+       'proleptic_gregorian': [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31],
+       'all_leap': [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31],
+       '366_day': [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31],
+       '360_day': [0, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30]}
+    
+def leap_year(year, calendar='standard'):
+    """Determine if year is a leap year
+    Args: 
+        year (numeric)
+    """
+    leap = False
+    if ((calendar in ['standard', 'gregorian',
+        'proleptic_gregorian', 'julian']) and
+        (year % 4 == 0)):
+        leap = True
+        if ((calendar == 'proleptic_gregorian') and
+            (year % 100 == 0) and
+            (year % 400 != 0)):
+            leap = False
+        elif ((calendar in ['standard', 'gregorian']) and
+                 (year % 100 == 0) and (year % 400 != 0) and
+                 (year < 1583)):
+            leap = False
+    return leap
+
+def get_days_per_mon(time, calendar='standard'):
+    """
+    return a array of days per month corresponding to the months provided in `months`
+    
+    Args: time (CFTimeIndex): ie. ds.time.to_index()
+          calendar (str): default 'standard'
+    """
+    month_length = np.zeros(len(time), dtype=np.int)
+
+    cal_days = dpm[calendar]
+
+    for i, (month, year) in enumerate(zip(time.month, time.year)):
+        month_length[i] = cal_days[month]
+        if leap_year(year, calendar=calendar):
+            month_length[i] += 1
+    return month_length
+
+def season_mean(ds, var, season = "all", cal = "none"):
+    """ calculate climatological mean by season
+    Args: ds (xarray.Dataset): dataset
+          var (str): variable to use
+          season (str): "all", 'DJF', "MAM", "JJA", "SON"
+          cal (str): "none"(default) or calendar used for weighting months by number of days
+    """
+    ## no weighting of months: 
+    if cal == "none":    
+        if season == "all": 
+            ## calculate mean for all season
+            smean = ds[var].groupby('time.season').mean('time')
+        else :
+            ## calculate mean for specified season
+            smean = ds[var].where(ds['time.season'] == season).mean('time')
+        
+        return smean
+    ## weighted months
+    else: 
+        ## create array of month_length (number of days in each month)
+        ## assign time coords matching original ds
+        month_length = xr.DataArray(get_days_per_mon(ds.time.to_index(), calendar=cal),
+                                 coords=[ds.time], name='month_length')
+        ## Calculate the weights by grouping by 'time.season'
+        weights = month_length.groupby('time.season') / month_length.groupby('time.season').sum()
+        
+        if season == "all": 
+            ## calculate weighted mean for all season
+            smean = (ds[var] * weights).groupby('time.season').mean('time')
+        else :
+            ## calculate weighted mean for specified season
+            smean = (ds[var] * weights).where(ds['time.season'] == season).mean('time')
+        
+        return smean
+
+def season_ts(ds, var, season):
+    """ calculate timeseries of seasonal averages
+    Args: ds (xarray.Dataset): dataset
+          var (str): variable to calculate 
+          season (str): 'DJF', 'MAM', 'JJA', 'SON'
+    """
+    ## set months outside of season to nan
+    ds_season = ds.where(ds['time.season'] == season)
+    
+    # calculate 3month rolling mean (only middle months of season will have non-nan values)
+    ds_season = ds_season[var].rolling(min_periods=3, center=True, time=3).mean()
+    
+    # reduce to one value per year
+    ds_season = ds_season.groupby('time.year').mean('time')
+    
+    # FUTURE: remove first year if it has nan?
+    return ds_season
 
